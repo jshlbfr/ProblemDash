a36ac514127fd2550c1d203e5ca2612e65d54cdd
Joshua Albufera <jshlbfr@Jshlbfrs-Mac-mini.local>
2026-08-18 17:45:23 +0800

    Initial ProblemDash frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    // UI
+    <div>
+      <h1>ProblemDash</h1>
+      <p>
+        A dashboard for tracking and managing problems. This will be my ongoing
+        portfolio project!
+      </p>
+    </div>
+  );
+}
+
+export default App;
